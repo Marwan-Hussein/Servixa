a7c1964f35a2a724d0804bf4e62e0a05cf5d970f
marwan hussein <marawanlabana649@gmail.com>
2026-04-27 22:21:23 +0300

    solve conflicts

--- Infrastructure/Servixa.Persistence/ProgarmService/DbService.cs
+++ Infrastructure/Servixa.Persistence/ProgarmService/DbService.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-<<<<<<< HEAD:Infrastructure/Servixa.Persistence/ProgarmService/DbService.cs
 using Microsoft.AspNetCore.Identity;
 using Servixa.Domain.Models.Users;
 using Servixa.Persistence.DbContext;
-=======
-using Servixa.Presistence.DbContext;
->>>>>>> parent of 5ce0ba4 (to add the first migration):Infrastructure/Servixa.Presistence/ProgarmService/DbService.cs
 using System;
 using System.Collections.Generic;
 using System.Linq;
